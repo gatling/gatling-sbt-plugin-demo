ce6045ce29ff38a6569aea62a566853e37773d2d
slandelle <slandelle@gatling.io>
2023-12-18 13:27:02 +0100

    chore: upgrade Gatling 3.10.0

--- src/test/scala/computerdatabase/ComputerDatabaseSimulation.scala
+++ src/test/scala/computerdatabase/ComputerDatabaseSimulation.scala
@@ -15,36 +15,30 @@ class ComputerDatabaseSimulation extends Simulation {
 
   val feeder = csv("search.csv").random
 
-  val search =
-    exec(
+  val search = exec(
       http("Home")
-        .get("/")
-    )
-      .pause(1)
-      .feed(feeder)
-      .exec(
-        http("Search")
-          .get("/computers?f=#{searchCriterion}")
-          .check(
-            css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
-          )
-      )
-      .pause(1)
-      .exec(
-        http("Select")
-          .get("#{computerUrl}")
-          .check(status.is(200))
-      )
-      .pause(1)
+        .get("/"),
+      pause(1),
+      feed(feeder),
+      http("Search")
+        .get("/computers?f=#{searchCriterion}")
+        .check(
+          css("a:contains('#{searchComputerName}')", "href").saveAs("computerUrl")
+        ),
+      pause(1),
+      http("Select")
+        .get("#{computerUrl}")
+        .check(status.is(200)),
+      pause(1)
+  )
 
   // repeat is a loop resolved at RUNTIME
   val browse =
   // Note how we force the counter name, so we can reuse it
-    repeat(4, "i") {
-      exec(
-        http("Page #{i}").get("/computers?p=#{i}")
-      ).pause(1)
-    }
+    repeat(4, "i")(
+      http("Page #{i}").get("/computers?p=#{i}"),
+      pause(1)
+    )
 
   // Note we should be using a feeder here
   // let's demonstrate how we can retry: let's make the request fail randomly and retry a given
@@ -52,29 +46,24 @@ class ComputerDatabaseSimulation extends Simulation {
 
   val edit =
   // let's try at max 2 times
-    tryMax(2) {
-      exec(
-        http("Form")
-          .get("/computers/new")
-      )
-        .pause(1)
-        .exec(
-          http("Post")
-            .post("/computers")
-            .formParam("name", "Beautiful Computer")
-            .formParam("introduced", "2012-05-30")
-            .formParam("discontinued", "")
-            .formParam("company", "37")
-            .check(
-              status.is { session =>
-                // we do a check on a condition that's been customized with
-                // a lambda. It will be evaluated every time a user executes
-                // the request
-                200 + ThreadLocalRandom.current().nextInt(2)
-              }
-            )
-        )
-    }
+    tryMax(2)(
+      http("Form").get("/computers/new"),
+      pause(1),
+      http("Post")
+        .post("/computers")
+        .formParam("name", "Beautiful Computer")
+        .formParam("introduced", "2012-05-30")
+        .formParam("discontinued", "")
+        .formParam("company", "37")
+        .check(
+          status.is { session =>
+            // we do a check on a condition that's been customized with
+            // a lambda. It will be evaluated every time a user executes
+            // the request
+            200 + ThreadLocalRandom.current().nextInt(2)
+          }
+    )
+    )
       // if the chain didn't finally succeed, have the user exit the whole scenario
       .exitHereIfFailed
 
